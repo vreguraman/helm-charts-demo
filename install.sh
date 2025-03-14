1a165df5390b49f69f26f9440ca091887f0b8ccf
vreguraman <96850237+vreguraman@users.noreply.github.com>
2025-03-14 13:59:41 +0000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+#Install Kind in UBUNTU
+
+https://kind.sigs.k8s.io/docs/user/quick-start/#installation
+
+# For AMD64 / x86_64
+[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.27.0/kind-linux-amd64
+# For ARM64
+[ $(uname -m) = aarch64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.27.0/kind-linux-arm64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
+
+apt install docker.io -y
+
+kind create cluster --name demo
